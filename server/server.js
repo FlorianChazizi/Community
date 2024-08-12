453e54004199e773021913cfbb8d3ab253696741
Florian Chazizi <chazizi.florian@gmail.com>
2024-08-13 00:19:47 +0300

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,35 +1,28 @@
-require('dotenv').config() // Load environment variables
-
 const express = require('express');
 const mongoose = require('mongoose');
 const connectDB = require('./config/database');
 const cors = require('cors');
 
-// routes imports 
-const userRoutes = require('./routes/user'); 
-const communityRoutes = require('./routes/community'); 
-const postRoutes = require('./routes/post')
+const userRoutes = require('./routes/user');
+const communityRoutes = require('./routes/community');
+const postRoutes = require('./routes/post');
 
 const app = express();
 app.use(cors());
-
 app.use(express.json());
 
-// path & method middleware
-app.use(( req, res, next) => {
-  console.log( req.path, req.method )
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
   next();
-})
+});
 
-//routes
 app.use('/api/user', userRoutes);
 app.use('/api/community', communityRoutes);
-app.use('/api/post', postRoutes)
+app.use('/api/post', postRoutes);
 
-// Connect to the database
 connectDB();
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('connected to db & listening on port', process.env.PORT)
-})
+app.get('/', (req, res) => res.send('API is running'));
+
+// Export the app for Vercel
+module.exports = app;
