740061e3f377ddd1a2da78f349b8a8438091315a
Florian Chazizi <chazizi.florian@gmail.com>
2024-08-13 00:29:01 +0300

    db config

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
-
-// connect to db
-const connectDB = async () => 
-  {
-    mongoose.connect(process.env.MONGO_URI)
-    console.log('connected in database.')
-  };
-  module.exports = connectDB;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('Database connected');
+  } catch (err) {
+    console.error('Database connection failed:', err);
+    process.exit(1);
+  }
+};
